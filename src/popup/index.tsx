05192c2fc5bb897bcb21b0c9ff8f78dcba330fef
mattywhitenz <128647429+mattywhitenz@users.noreply.github.com>
2024-01-10 16:51:25 +1100

    Update index.tsx

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,4 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import { App } from './app'
+import { createRoot } from 'react-dom/client';
+import { App } from './app';
 
-createRoot(document.getElementById('app')).render(<App />)
+// This function will be called to render the App after a delay
+function renderApp() {
+    const container = document.getElementById('app');
+    const root = createRoot(container!); // the '!' is a non-null assertion telling TypeScript that 'container' will not be null
+    root.render(<App />);
+}
+
+// Wait for 5 seconds before rendering the App
+setTimeout(renderApp, 5000);
